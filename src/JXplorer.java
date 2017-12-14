5b36ab2d50005c80a0a2862dd9283b5b3a396abf
Khaynar <jlphillips1992@gmail.com>
2017-12-14 20:48:08 +0100

    Finished filling JXplorer.java
    Completed the page JXplorer with Isabel

--- src/JXplorer.java
+++ src/JXplorer.java
@@ -1,52 +1,38 @@
-import java.util.ArrayList;
 import java.util.Arrays;
 
 public class JXplorer {
 
-    //TODO this is a good start, but remember, the syntax for a variable is Type name. 
-    //What do you think would be a good name for the file (folder) being looked at?
-    //Hint: look at the getters and setters. 
-    private JXploreFile;
-    
-    //TODO 'linking private and public' isn't a thing. The fields are characteristics of the class, like a car has a colour. 
-    //Does a JXplorer need a JXplorer? 
-    //It may be possible, a person has two parents, who are both persons again. 
-    //'private Human parent' would make sense for drawing up a family tree. 
-    //Tip: think of what it's relation is before you assign a class (type) to it. 
-    private JXplorer;
+    private JXploreFile currentFile;
 
     public JXplorer() {
-        //TODO
+        this.currentFile = new JXploreFile();
     }
 
     public JXplorer(JXploreFile file) {
-        //TODO
+        this.currentFile = file;
     }
 
     public JXploreFile getCurrentFile() {
-        //TODO
-        return null;
+        return this.currentFile;
     }
 
     public void setCurrentFile(JXploreFile file) {
-        //TODO
+        this.currentFile = file;
     }
 
     public void printName(JXploreFile file) {
-        //TODO BITCHES
-
+        System.out.println(file.getName());
     }
 
     public void printSubFiles(JXploreFile file) {
         System.out.println(Arrays.toString(file.getSubFiles()));
     }
 
-      public void printSubFolders(JXploreFile file) {
-        //TODO GENJI
+    public void printSubFolders(JXploreFile file) {
+        System.out.println(Arrays.toString(file.getSubFiles()));
     }
 
     public static void main(String[] args) {
-        JXploreFile file = new JXploreFile();
-        System.out.println(file.getPath());
+        new JXplorer();
     }
 }
